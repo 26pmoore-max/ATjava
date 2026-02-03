69979e825b8da73e397d75de216d66f63cd75161
26pmoore-max <26pmoore@derryfield.org>
2026-02-03 11:08:25 -0500

    Test

---
+++ Practical Portion of Exam/Part1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class Part1 {
+
+    public static void main(String[] args) {
+        int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        swap(2, 7, numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+    public static void swap(int i, int j, int[] arr) {
+        if (i >= 0 && i < arr.length && j >= 0 && j < arr.length) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
